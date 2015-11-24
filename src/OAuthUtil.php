f356d9a36f547efc7b21e76f8ba5eae1a5dd5ce0
Paul Quinn <paul.mark.quinn@googlemail.com>
2015-11-24 16:29:21 +0000

    fix an issue in OAuthUtil

--- src/OAuthUtil.php
+++ src/OAuthUtil.php
@@ -2,10 +2,12 @@
 
 namespace Vzaar;
 
+use Exception;
+
 class OAuthUtil {
     public static function urlencode_rfc3986($input) {
         if (is_array($input)) {
-            return array_map(array('OAuthUtil', 'urlencode_rfc3986'), $input);
+            return array_map(array('\\Vzaar\\OAuthUtil', 'urlencode_rfc3986'), $input);
         } else if (is_scalar($input)) {
             return str_replace(
                 '+',
